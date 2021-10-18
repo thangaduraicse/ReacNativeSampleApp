568751f6851ddfb9e60ccd87d775e6e83ae4262e
Thangadurai Nainamalai <Thangadurai.Nainamalai@freshworks.com>
2021-10-18 22:12:37 +0530

    Updated app.js and package.json

--- App.js
+++ App.js
@@ -1,42 +1,82 @@
-import React, { useState,useRef } from 'react';
-import { View,Button,StyleSheet, Platform} from 'react-native';
-import { WebView } from 'react-native-webview';
-import { getUniqueId } from 'react-native-device-info';
+import React, { useRef } from "react";
+import {
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from "react-native";
+import { WebView } from "react-native-webview";
+import { getUniqueId } from "react-native-device-info";
+import LoremIpsum from "./LoremIpsum";
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "ios" ? 20 : 0,
+  },
+  chatIcon: {
+    width: 75,
+    height: 75,
+    backgroundColor: "#FF9800",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "fixed",
+    top: 60,
+    right: 10,
+    borderRadius: 37.5,
+    zIndex: 1,
+  },
+});
 
-const App = ()=> {
-  let id = getUniqueId();
+const App = () => {
   const webViewRef = useRef(null);
-  const [showWebView,setShowWebView] = useState(false);
-  const runFirst = `
-    setData('${id}');
-    true; // note: this is required, or you'll sometimes get silent failures
-  `;
+  const sampleUserID = "test@amanco.com";
+  const sampleOrderID = "12345";
+
+  const handleChatButtonClick = () => {
+    const uniqueId = getUniqueId();
+
+    // Refe showFreshbotsWidget in freshbots-delayed.html
+    webViewRef.current?.injectJavaScript(`
+      window.Freshbots &&
+        window.showFreshbotsWidget && 
+        window.showFreshbotsWidget(${sampleUserID}, ${sampleOrderID}, ${uniqueId});
+    `);
+  };
+
+  /*
+   * for android we recommend hosting it and using the static HTML
+   * https://github.com/react-native-webview/react-native-webview/issues/746
+   * https://stackoverflow.com/questions/30061363/access-denied-using-local-storage-in-android-webview/47832246
+   *
+   */
+
   return (
     <View style={styles.container}>
-      {/*
-        * for android we recommend hosting it and using the static HTML
-        * https://github.com/react-native-webview/react-native-webview/issues/746
-        * https://stackoverflow.com/questions/30061363/access-denied-using-local-storage-in-android-webview/47832246
-        * 
-        * */
-      }
-      {showWebView ? <WebView
-        source={{uri: "https://616d90d5864d5.htmlsave.net"}}
+      <TouchableHighlight
+        onPress={handleChatButtonClick}
+        style={styles.chatIcon}
+      >
+        <Text>Chat</Text>
+      </TouchableHighlight>
+      <ScrollView>
+        <LoremIpsum />
+      </ScrollView>
+      <WebView
         ref={webViewRef}
-        onMessage={(event)=>{}}
-        injectedJavaScript={runFirst}
-      /> : null}
-      <Button onPress={()=>{setShowWebView(true)}} title='Open Widget'></Button>
-      <Button onPress={()=>{setShowWebView(false)}} title='Close'></Button>
+        source={
+          Platform.OS === "android"
+            ? {
+                uri: "file:///android_asset/freshbots-delayed.html"
+                // We put freshworks.html in "android/app/src/main/assets" using cp command. Refer: package.json -> scripts -> android
+              }
+            : require("./freshbots-delayed.html")
+        }
+      />
     </View>
   );
-  }
+};
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-  });
 export default App;
